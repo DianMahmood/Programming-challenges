093a12fbfdd06e3b40cc9b0c3a386435166d9ec5
DianMahmood <112864800+DianMahmood@users.noreply.github.com>
2022-09-06 10:27:52 +0100

    Add files via upload

--- Brampton Manor Academy/1/Rod_Conversions.py
+++ Brampton Manor Academy/1/Rod_Conversions.py
@@ -6,19 +6,25 @@ def conversion_meters(rods):
 def conversion_feet(rods):
     meters = rods * 5.0292
     feet = meters/0.3048
+    return feet
 
 
 def conversion_miles(rods):
     meters = rods * 5.0292
     miles = meters/1609.34
+    return miles
 
 
 def conversion_furlongs(rods):
     furlongs = rods/40
+    return furlongs
 
 
 def conversion_minutes(rods):
-    pass
+    meters = rods * 5.0292
+    miles = meters/1609.34
+    minutes = (miles/3.1) * 60
+    return minutes
 
 
 
@@ -26,4 +32,23 @@ def conversion_minutes(rods):
 
 rods = float(int(input("Input rods: ")))
 print(f"You input {rods} rods")
+print(f"Conversions:\nMeters: {conversion_meters(rods)}\nFeet: {conversion_feet(rods)}\nMiles: {conversion_miles(rods)}\nFurlongs: {conversion_furlongs(rods)}")
+print(f"Minutes to walk {rods} rods: {conversion_minutes(rods)}")
+
+
+
+
+
+
+
+
+
+
+
+             
+
+
+
+
+
 
