313a495efd673babc4a0a38b5cc31ee661962f30
DianMahmood <112864800+DianMahmood@users.noreply.github.com>
2022-09-07 20:18:37 +0100

    Update Challenge 1.py

--- Codewars/Brampton Manor Academy/Challenge 1.py
+++ Codewars/Brampton Manor Academy/Challenge 1.py
@@ -1,33 +1,25 @@
 def conversion_meters(rods):
-    meters = rods * 5.0292
-    return meters
+    return rods * 5.0292
 
 
 def conversion_feet(rods):
-    meters = rods * 5.0292
-    feet = meters/0.3048
-    return feet
+    return conversion_meters(rods) / 0.3048
 
 
 def conversion_miles(rods):
-    meters = rods * 5.0292
-    miles = meters/1609.34
-    return miles
+    return conversion_meters(rods) / 1609.34
 
 
 def conversion_furlongs(rods):
-    furlongs = rods/40
-    return furlongs
+    return rods/40
 
 
 def conversion_minutes(rods):
-    meters = rods * 5.0292
-    miles = meters/1609.34
-    minutes = (miles/3.1) * 60
-    return minutes
+    return (conversion_miles(rods) / 3.1) * 60
 
 
 rods = float(int(input("Input rods: ")))
+
 print(f"You input {rods} rods")
-print(f"Conversions:\nMeters: {conversion_meters(rods)}\nFeet: {conversion_feet(rods)}\nMiles: {conversion_miles(rods)}\nFurlongs: {conversion_furlongs(rods)}")
+print(f"\nConversions:\nMeters: {conversion_meters(rods)}\nFeet: {conversion_feet(rods)}\nMiles: {conversion_miles(rods)}\nFurlongs: {conversion_furlongs(rods)}")
 print(f"Minutes to walk {rods} rods: {conversion_minutes(rods)}")
