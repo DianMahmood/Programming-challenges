1f31ea071a241f11becac6b5ae18547fd9632833
DianMahmood <112864800+DianMahmood@users.noreply.github.com>
2023-01-31 10:54:48 +0000

    Add files via upload

---
+++ CasinoPython.py
@@ -0,0 +1,79 @@
+import random
+
+
+class Outcome:
+    """
+    Each number has a variety of Outcome i.e. ("Red" 1:1)
+    Test: test = Outcome("test",5), then test.winAmount(5) = 25
+    Chapter 5, pages 37-43
+    """
+    def __init__(self, name: str, odds: int):
+        self.name = str(name)
+        self.odds = int(odds)
+
+
+    def winAmount(self, amount:float) -> float:
+        return self.odds * amount
+
+
+    def __eq__(self, other) -> bool:
+        return self.name == other.name
+
+
+    def __ne__(self, other) -> bool:
+        return self.name != other.name
+
+
+    def __hash__(self) -> int:
+        return hash(self.name)
+
+
+    def __str__(self):
+        return "{name:s} ({odds:d}:1)".format_map(vars(self))
+
+
+    def __repr__(self):
+        return "{class_:s}({name!r}, {odds!r})".format(
+            class_=type(self).__name__, **vars(self))
+
+
+class Bin(frozenset):
+    """
+    Each bin will have 12-14 Outcomes
+    Frozenset means a unique values, fixed once created
+    Extended (inheirts all methods from frozenset e.g. .add())
+    Chapter 6, pageas 45-48
+    """
+    pass
+
+
+
+class Wheel:
+    """
+    Contains 38 bins
+    Responsible for Random Number Generation (RNG)
+    Chapter 7, pages 49-52
+    """
+    def __init__(self):
+        self.bins = list(Bin() for _ in range(38))
+        self.rng = random.Random()
+        self.rng.seed(4)
+
+
+    def addOutcome(self, number: int, outcome):
+        self.bins[number] = Bin(self.bins[number] | Bin[outcome])
+
+
+    def next(self):
+        return self.rng.randint(0, 37) #TODO
+
+
+    def get(self, bin):
+        return self.bins[bin]
+
+
+w = Wheel()
+print(w.next())
+print(w.next())
+print(w.next())
+
